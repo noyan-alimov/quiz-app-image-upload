2d0405360b3517c40b16a5673f75eb3fc50f5f0c
Noyan <noyan.alimov@gmail.com>
2021-03-04 19:17:27 +0000

    setup docker and k8s

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,23 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/putSignedUrl/:fileName', async (req: Request, res: Response) => {
-	let { fileName } = req.params;
-	const url = getPutSignedUrl(fileName);
-	res.status(201).send({ url });
-});
+app.get(
+	'/api/v0/image-upload/putSignedUrl/:fileName',
+	async (req: Request, res: Response) => {
+		let { fileName } = req.params;
+		const url = getPutSignedUrl(fileName);
+		res.status(201).send({ url });
+	}
+);
 
-app.get('/getSignedUrl/:fileName', async (req: Request, res: Response) => {
-	let { fileName } = req.params;
-	const url = getGetSignedUrl(fileName);
-	res.status(200).send({ url });
-});
+app.get(
+	'/api/v0/image-upload/getSignedUrl/:fileName',
+	async (req: Request, res: Response) => {
+		let { fileName } = req.params;
+		const url = getGetSignedUrl(fileName);
+		res.status(200).send({ url });
+	}
+);
 
 app.listen(8080, () => {
 	console.log(`Running on port: 8080`);
